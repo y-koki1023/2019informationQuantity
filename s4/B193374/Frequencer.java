442d1b59c9396c959888e2b206c9da3164efd04b
KokiYamamoto <y.koki1023@gmail.com>
2019-12-17 16:29:33 +0900

    [Fix] Frequencer int Frequency()未実装部分を実装 エラー処理 コンストラクタを実装

--- s4/B193374/Frequencer.java
+++ s4/B193374/Frequencer.java
@@ -16,23 +16,30 @@ interface FrequencerInterface {     // This interface provides the design for fr
 
 
 public class Frequencer implements FrequencerInterface{
-    // Code to Test, *warning: This code  contains intentional problem*
+	// Code to Test, *warning: This code  contains intentional problem*
+	public Frequencer(byte [] target, byte []space){
+		this.setTarget(target);
+		this.setSpace(space);
+	}
     byte [] myTarget;
     byte [] mySpace;
     public void setTarget(byte [] target) { myTarget = target;}
     public void setSpace(byte []space) { mySpace = space; }
     public int frequency() {
-	int targetLength = myTarget.length;
-	int spaceLength = mySpace.length;
-	int count = 0;
-	for(int start = 0; start<spaceLength; start++) { // Is it OK?
-	    boolean abort = false;
-	    for(int i = 0; i<targetLength; i++) {
-			if(myTarget[i] != mySpace[start+i]) { abort = true; break; }
-	    }
-	    if(abort == false) { count++; }
-	}
-	return count;
+		if(myTarget.equals(null) || myTarget.length == 0 ) return -1;
+		if(mySpace.equals(null) || mySpace.length == 0) return 0;
+		int targetLength = myTarget.length;
+		int spaceLength = mySpace.length;
+		
+		int count = 0;
+		for(int start = 0; start<spaceLength; start++) { // Is it OK?
+			boolean abort = false;
+			for(int i = 0; i<targetLength; i++) {
+				if(myTarget[i] != mySpace[start+i]) { abort = true; break; }
+			}
+			if(abort == false) { count++; }
+		}
+		return count;
     }
 
     // I know that here is a potential problem in the declaration.
